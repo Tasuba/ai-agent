2a412e28f3c272543a5fe9c2c99d94295b008065
Tasuba <Brokensky585@gmail.com>
2025-07-23 20:56:26 -0400

    Removed hardcoded input, added ability to accept a prompt through CLI

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    verbose = False
+    model = "gemini-2.0-flash-001"
     _ = load_dotenv()
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    model = "gemini-2.0-flash-001"
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    resp = client.models.generate_content(model=model, contents=contents)
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        verbose = True
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
+    resp = client.models.generate_content(model=model, contents=messages)
     print(resp.text)
-    if resp.usage_metadata:
+    if verbose and resp.usage_metadata:
+        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {resp.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {resp.usage_metadata.candidates_token_count}")
 
